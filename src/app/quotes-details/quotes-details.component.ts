04153a0f70244b85e3c9fc20a71c3d5eca14c36e
leaclaire <leah.kiplagat@student.moringaschool.com>
2021-05-15 23:35:02 +0300

    creating an array

--- src/app/quotes-details/quotes-details.component.ts
+++ src/app/quotes-details/quotes-details.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quotes-details',
   templateUrl: './quotes-details.component.html',
   styleUrls: ['./quotes-details.component.css']
 })
 export class QuotesDetailsComponent implements OnInit {
-
+  @Input() quoty: Quote;
+  @Output() isRead = new EventEmitter<boolean>();
+  deleteQuote(read:boolean){
+    this.isRead.emit(read);
+  }
   constructor() { }
 
   ngOnInit(): void {
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes',
@@ -6,7 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-
+  quotes:Quote[] = [
+    new Quote (1,'Leah','morality','Being honest,loving and caring in the society lies on the hands of every citizen.','Nelson Mandela', new Date(2019,7,12),0,0),
+    new Quote (2,'Nicholus','Technology','Any sufficiently advanced technology is indistinguishable from magic.','Arthur C. Clarke',new Date(2019,7,14),0,0),
+    new Quote (3,'Lexia','Citizens','Never doubt a small group of thoughtfull citizen can change the world ,it is the only thing that ever has.','BrainQuote',new Date(2019,6,3),0,0),
+    new Quote (4,'Festus','Business',' Let your plans be dark and impenetrable as night, and when you move, fall like a thunderbolt.','Sun Tzu',new Date(2019,7,7),0,0),
+    new Quote (5,'Alex','Religion','Anyone who thinks sitting in church can make you a Christian must also think that sitting in a garage can make you a car.','Garrison Keillor',new Date(2019,7,17),0,0),
+    new Quote (6,'ken','seasons','n the depth of winter, I finally learned that within me there lay an invincible summer.',' Albert Camus',new Date(2021,4,15),0,0)
+  ];
+  get sortQuotes() {
+    return this.quotes.sort((a, b) => {
+      return <any>new Date(b.datePosted) - <any>new Date(a.datePosted);
+    });
+  }  
+  addedQuote(quote){
+    let arraysize = this.quotes.length;
+    quote.id = arraysize+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
+  quoteDelete(isRead, index){
+    if (isRead) {
+      let toDelete = confirm(`Are you sure you want to delete this Quote?`)
+      if(toDelete){
+        this.quotes.splice(index,1);
+      }
+      
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
