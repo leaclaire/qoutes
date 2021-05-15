3c2e7f02c840307626953eee0b260a09ff57d842
leaclaire <leah.kiplagat@student.moringaschool.com>
2021-05-15 22:09:20 +0300

    adding id and quotes.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,4 @@
 export class Quote {
+    constructor(public id:number,public name:string,public quoteTitle:string,public quoteInfo:string,public author:string, public datePosted: Date,public upvote:number, public downvote:number){
+    }
 }
